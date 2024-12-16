b379ff8e5f7cc0db18c8a03a2f395c8c55ac04ed
DESKTOP-M5CUFVP\admin <lnam061@gmail.com>
2024-12-16 17:07:59 +0900

    임시 영화 추천 페이지 추

--- WDWDN/src/main/java/com/mbti/controller/HomeController.java
+++ WDWDN/src/main/java/com/mbti/controller/HomeController.java
@@ -36,4 +36,28 @@ public class HomeController {
 	        return "home.html"; // home.html로 이동
 	        
 	    }
+	    
+	    @GetMapping("/list1.html")
+		public String listpage(Model model, Principal principal) {
+			 Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		        String username = (authentication != null && authentication.getPrincipal() instanceof User)
+		                ? ((User) authentication.getPrincipal()).getUsername()
+		                : null;
+		        model.addAttribute("username", username); 
+		        return "list1.html"; // home.html로 이동
+		        
+		    }
+		
+	    
+	    @PostMapping("/list1.html")
+	    public String listPost(@RequestParam("extraData") String extraData, 
+	                               Model model, 
+	                               Principal principal) {
+	        String username = principal.getName(); // 로그인된 유저 이름
+	        model.addAttribute("username", username); // 모델에 추가
+	        model.addAttribute("extraData", extraData); // POST 요청으로 받은 데이터 추가
+	       // System.out.println("Username: " + username);
+	        return "list1.html"; // home.html로 이동
+	        
+	    }
 	}
